e4eeb9f873ce4e1377eee7ca6ecc80e49e45d2e9
Ayah-Khawaldeh <Ayah.m.khawaldeh@gmail.com>
2023-05-22 07:59:13 +0300

    x

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+
+class Article:
+    def __init__(self, headline, content):
+        self.headline = headline
+        self.content = content
 
 
 def main():
@@ -73,6 +80,20 @@ def main():
         line = file.readline()
     driver.execute_script(f"arguments[0].value = '{line}';", textarea)
 
+    pickers = WebDriverWait(driver,10).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR,'divelem.-selectbox'))
+    )
+
+
+    driver.execute_script("arguments[0].click",pickers[0])
+    arabic_choice = pickers[0].find_element(By.XPATH,'//divelem[@data-widget-value="ar"]')
+    print(arabic_choice)
+    driver.execute_script("arguments[0].click",arabic_choice)
+
+    suggestion = pickers[1].find_element(By.XPATH,'//divelem[@data-widget-value="3"]')
+    print(suggestion)
+    driver.execute_script("arguments[0].click", suggestion)
+
     # Find the custom element using its attribute values
     button = WebDriverWait(driver,10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR,"generate-button.hoverable.activable"))
@@ -81,34 +102,85 @@ def main():
     # Execute JavaScript to click on the custom element
     driver.execute_script("arguments[0].click();", button)
 
+
     # Switch back to the main window
     driver.switch_to.window(driver.window_handles[0])
 
     # # Close the popup window
     # driver.close()
 
-    wait = WebDriverWait(driver, 30)
+    wait = WebDriverWait(driver, 60)
     wait.until(EC.url_contains('https://katteb.com/ar/dashboard/activities/'))
 
     print('url changed to done genration')
 
     elements = driver.find_element(By.CSS_SELECTOR,'div.fr-element.fr-view')
-    paragraph = elements.find_element(By.TAG_NAME,'p')
+    paragraphs = elements.find_elements(By.TAG_NAME,'p')
     # Remove numbers and dots, and split the text into lines
-    lines = [line.strip().strip("1234567890.-[] ") for line in paragraph.text.splitlines() if line.strip()]
-
+    titles = []
+    for paragraph in paragraphs:
+        lines = [line.strip().strip("1234567890.-[] ") for line in paragraph.text.splitlines() if line.strip()]
+        titles.extend(lines)
     # Print each line and store them in an array
     formatted_lines = []
-    for line in lines:
-        formatted_lines.append(line)
+    with open('titles.txt', mode='w',encoding='utf-8') as titles_file:
+        titles_file.write('\n'.join(titles))
 
 
     driver.get('https://katteb.com/ar/dashboard/generate-full-article/')
     form = driver.find_elements(By.TAG_NAME,'multistep-form-body-field')
 
-    form[0].click()
+    title = form[0].click()
+    form[0].find_element(By.NAME, 'topic_title').send_keys('this is test keys 0')
 
-    print(form)
+
+    title = form[1].click()
+    arabic_option = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'multistep-form-body-field-fill-selectbox-item[data-value="ar"]')))
+    arabic_option.click()
+
+    title = form[2].click()
+    search = driver.find_elements(By.CSS_SELECTOR,'input.-multistep-selectbox-search')[-1]
+    driver.execute_script("arguments[0].value = 'Jordan';", search)
+    search.send_keys(Keys.ENTER)
+    jordan_aud = driver.find_element(By.CSS_SELECTOR, 'multistep-form-body-field-fill-selectbox-item[data-value="JO"')
+    jordan_aud.click()
+
+    title = form[3].click()
+    numbers_of_lines = driver.find_element(By.ID, 'topic_numberofwords')
+    driver.execute_script("arguments[0].value = 1200", numbers_of_lines)
+
+    next_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.TAG_NAME, "multistep-form-next"))
+    )
+
+    next_button.click()
+
+    write_button = WebDriverWait(driver,40).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR,'div.-start-generating-button.hoverable.activable'))
+    )
+
+    write_button.click()
+
+    show_article = WebDriverWait(driver, 600).until(
+        EC.presence_of_element_located((By.LINK_TEXT, 'عرض المقال'))
+    )
+
+    show_article.click()
+
+
+    articles_holder = WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR,
+                                      'div.fr-element.fr-view'))
+    )  # Replace 'your_div_id' with the actual ID of the div element
+
+    p_elements = articles_holder.find_elements(By.TAG_NAME,'p')
+
+    article = Article(headline=p_elements[0],content=str(articles_holder.text).replace(p_elements[0].text, ''))
+
+    print(article.headline)
+    print(article.content)
+
 
 
 
